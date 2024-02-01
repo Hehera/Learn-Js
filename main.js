eb9193d259bc677e73f98c7b91057de6be68c5af
Гегера Назар <lolerkapich335@gmail.com>
2024-02-01 11:20:48 +0200

    Chapter Recap Using an if condition, you can run a piece of code when the condition evaluates to true The syntax is if (condition) and then curly braces {} wrap the lines of code that correspond to this condition The else keyword can be used to perform some other code based on all the other conditions not satisfied with the if. When you have an if/else condition that returns two different results, it is possible to drop the else keyword. Always use triple equals (===) when comparing 2 values in JavaScript.

--- main.js
+++ main.js
@@ -10,11 +10,39 @@ function capitalize(word) {
       }
    }
    return res
-
-   
-
 }
 
-// console.log(capitalize('sam')) // "Sam"
-// console.log(capitalize('aLEX')) // "Alex"
-// console.log(capitalize("chARLie")); // "Charlie"
+function getDescription(text) {
+   if (text.length <= 10) {
+      return text
+   } else {
+      let res = ''
+      for (let i = 0; i < 10; i++) {
+         res += text[i]
+      }
+      return res + '...'
+   }
+}
+// console.log(getDescription('Welcome to hello'));
+
+
+function isPassing(grade) {
+    return grade >= 10;
+}
+// console.log(isPassing(11));
+
+
+function evenOrOdd(number) {
+    if(number % 2 === 0) {
+        return 'even'
+    } else {
+        return 'odd'
+    }
+}
+
+console.log(evenOrOdd(4));
+
+
+
+
+
