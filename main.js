57927bb0dd8414787f852fa3a03e8f448650bc04
Гегера Назар <lolerkapich335@gmail.com>
2024-02-01 10:19:59 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function capitalize(word) {
+   let res = ''
+
+   for (let i = 0; i < word.length; i++) {
+      if (i === 0) {
+         res += word[i].toUpperCase()
+        } else {
+          res += word[i].toLowerCase()
+      }
+   }
+   return res
+}
+
+// console.log(capitalize('sam')) // "Sam"
+// console.log(capitalize('aLEX')) // "Alex"
+// console.log(capitalize("chARLie")); // "Charlie"
